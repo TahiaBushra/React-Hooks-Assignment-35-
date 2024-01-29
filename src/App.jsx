8e11d715a715201085815593c56ada6a5932ef6c
TahiaBushra <bushratahia@gmail.com>
2024-01-29 22:39:30 +0600

    Data Count Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
-// purposeful updating phase
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 const App = () => {
-  const [fullName, setFullName] = useState("");
-  const [error, setError] = useState(false);
-  const handleFullName = () => {
-    setFullName("Mimi");
-  };
-  const handleError = () => {
-    setError(true);
-  };
-  if (!error) {
-    return (
-      <div>
-        <p>Assalamualaikum, {fullName}.</p>
-        <button onClick={handleFullName}>Popup Name</button>
-        <button onClick={handleError}>Create Error</button>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <p>An Error Occured, Please Refresh The Page.</p>
-      </div>
-    );
-  }
+  const [count, setCount] = useState();
+  useEffect(() => {}, []);
+  return (
+    <div>
+      <p>Count:{count}</p>
+      <button onClick={() => setCount(count + 1)}>Add</button>
+    </div>
+  );
 };
 
 export default App;
